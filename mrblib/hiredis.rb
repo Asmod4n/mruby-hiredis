2708e648b6216563fda7f341ac8b44f48c4c6199
Asmod4n <Asmod4n@users.noreply.github.com>
2015-12-02 19:31:29 +0100

    add public method to create shortcuts

--- mrblib/hiredis.rb
+++ mrblib/hiredis.rb
@@ -1,19 +1,23 @@
 class Hiredis
   class << self
-    attr_accessor :initialized
+    attr_accessor :created_shortcuts
 
     def new(*args)
       instance = super(*args)
-      unless self.initialized
-        instance.call(:command).each do |command|
-          command = command.first.to_sym
-          define_method(command) do |*args|
-            call(command, *args)
-          end
-        end
-        self.initialized = true
+      unless self.created_shortcuts
+        create_shortcuts(instance)
+        self.created_shortcuts = true
       end
       instance
+    end
+
+    def create_shortcuts(hiredis)
+      hiredis.call(:command).each do |command|
+        command = command.first.to_sym
+        define_method(command) do |*args|
+          call(command, *args)
+        end
+      end
     end
   end
 
