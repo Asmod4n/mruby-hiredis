e58a078bb41fc999cf94aeaf856904108f7de171
Asmod4n <Asmod4n@users.noreply.github.com>
2015-12-02 19:02:25 +0100

    add shortcut funcs for redis commands

--- mrblib/hiredis.rb
+++ mrblib/hiredis.rb
@@ -1,4 +1,22 @@
 class Hiredis
+  class << self
+    attr_accessor :initialized
+
+    def new(*args)
+      instance = super(*args)
+      unless self.initialized
+        instance.call(:command).each do |command|
+          command = command.first.to_sym
+          define_method(command) do |*args|
+            call(command, *args)
+          end
+        end
+        self.initialized = true
+      end
+      instance
+    end
+  end
+
   def multi(*commands)
     append(:multi)
     commands.each do |command|
@@ -10,5 +28,13 @@ class Hiredis
       ret << reply
     end
     ret
+  end
+
+  def [](key)
+    call(:get, key)
+  end
+
+  def []=(key, value)
+    call(:set, key, value)
   end
 end
