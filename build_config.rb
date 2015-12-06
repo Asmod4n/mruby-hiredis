f28df1ed11bc0e33a7857c7718927a15de49207a
Asmod4n <Asmod4n@users.noreply.github.com>
2015-12-06 13:58:27 +0100

    enable debug build

--- build_config.rb
+++ build_config.rb
@@ -1,6 +1,8 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
+  enable_debug
+  conf.cc.defines = %w(ENABLE_DEBUG)
+  conf.enable_test
   conf.gembox 'full-core'
   conf.gem File.expand_path(File.dirname(__FILE__))
-  conf.enable_test
 end
